432b0902e019bdf5f4a3e30d1b7d3cb3f039de23
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-05-28 09:44:30 +0300

    chore:Add payment processing with stock validation

--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -1,5 +1,6 @@
 const Payment = require('../model/Payment');
 const Coffee = require('../model/Coffee');
+const Stock = require('../model/Stock');
 
 exports.createPayment = async (req, res) => {
   const { method, amount, coffeeId } = req.body;
@@ -11,6 +12,21 @@ exports.createPayment = async (req, res) => {
     if (amount < coffee.price) {
       return res.status(400).json({ message: 'Paiement insuffisant' });
     }
+    const stock = await Stock.findOne();
+    if (!stock) return res.status(500).json({ message: 'Stock non initialisé' });
+
+     if (
+        stock.coffeeBeans < coffee.coffeeBeans ||
+        stock.milk < coffee.milk ||
+        stock.sugar < coffee.sugar
+      ) {
+          return res.status(400).json({ message: 'Stock insuffisant pour préparer ce café' });
+        }
+        stock.coffeeBeans -= coffee.coffeeBeans;
+        stock.milk -= coffee.milk;
+        stock.sugar -= coffee.sugar;
+
+        await stock.save();
 
     const payment = new Payment({ method, amount, coffeeId });
     await payment.save();
