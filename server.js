e0d551ea127c5f953821e8cf24f1fe4cecf967f0
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-05-28 09:19:49 +0300

    chore: use payment router

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const coffeeRoutes = require('./src/routes/coffeeRoutes');
+const paiementRoutes = require('./src/routes/paymentRoutes');
 
 const app = express();
 
@@ -20,7 +21,7 @@ mongoose.connect('mongodb://localhost:27017/taskmanager', {
 app.use(express.json());
 
 app.use('/api/coffees', coffeeRoutes);
-
+app.use('/api/payment', paiementRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur port ${PORT}`);
